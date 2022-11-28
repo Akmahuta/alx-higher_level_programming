e89de85dd4beeb2f5b48b6aac52555c6f69059cf
Abdulkadir <mahuta2012@gmail.com>
2022-11-28 08:36:41 -0800

    Refactored code

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
-const cFun = 'C is fun';
-const pyCool = 'Python is cool';
-const jS = 'JavaScript is amazing';
-
-console.log(cFun);
-console.log(pyCool);
-console.log(jS);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
 
