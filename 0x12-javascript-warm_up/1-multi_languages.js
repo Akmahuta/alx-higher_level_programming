64f3f603885873f0789dc18f2fcf9f789152e271
Abdulkadir <mahuta2012@gmail.com>
2022-11-28 07:46:30 -0800

    correction

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const cFun = 'C is fun'
-const pyCool = 'Python is cool'
-const jS = 'JavaScript is amazing'
+const cFun = 'C is fun';
+const pyCool = 'Python is cool';
+const jS = 'JavaScript is amazing';
 
-console.log(cFun)
-console.log(pyCool)
-console.log(jS)
+console.log(cFun);
+console.log(pyCool);
+console.log(jS);
 
