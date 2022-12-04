0058c3e6950cdb630b879ccd8568f3475f3ebf3e
Abdulkadir <mahuta2012@gmail.com>
2022-12-04 03:15:56 -0800

    task 2 fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of the database hbtn_0e_0_usa
-whose name matches that supplied as argument.
-Usage: ./2-my_filter_states.py <mysql username> <mysql password> 
-<database name> <state name searched>
+Takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
 """
+
 import MySQLdb
 from sys import argv
 
